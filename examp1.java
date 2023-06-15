54517a6332253e1ee159d6890f20fc9deaf455c2
Sukesh S <105391825+Sukesh22@users.noreply.github.com>
2023-06-16 01:50:36 +0530

    Add files via upload

---
+++ examp1.java
@@ -0,0 +1,5 @@
+class examp1 {
+    public static void main(String[] args) {
+        System.out.println("*");
+    }
+}
---
+++ examp2.java
@@ -0,0 +1,8 @@
+public class examp2 {
+    public static void main(String[] args) {
+        int j;
+        for (j = 1; j <= 5; j++) {
+            System.out.println("*");
+        }
+    }
+}
